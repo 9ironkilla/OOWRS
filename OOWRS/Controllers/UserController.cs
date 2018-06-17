c5b815b05b26f259b709e3628e155f1caf72942a
EVW <dev@bareme.me>
2018-06-16 20:00:12 -0700

    Add project files.

---
+++ OOWRS/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OOWRS.Models;
+using OOWRS.Repository;
+
+
+namespace OOWRS.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserRepository _userRepository = new UserRepository();
+
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(UserModel user)
+        {
+            if(_userRepository.FindUser(user) != null)
+            {
+
+                HttpCookie cookie = HttpContext.Request.Cookies["UserCookie"] ?? new HttpCookie("UserCookie ");
+                cookie.Values["UserName"] = user.Name;
+                cookie.Expires = DateTime.Now.AddDays(1);
+                HttpContext.Response.Cookies.Add(cookie);
+
+                return View("~/Views/Home/Index.cshtml");
+            }
+            else
+            {
+                return View("LoginError");
+            }
+        }
+    }
+}
---
+++ OOWRS/Repository/UserRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OOWRS.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace OOWRS.Repository
+{
+    public interface IUserRepository
+    {
+        UserModel FindUser(UserModel user);
+
+    }
+
+    public class UserRepository : IUserRepository
+    {
+
+
+        private SqlConnection _UserConnection;
+        private string _FindUserQuery = "  select * from [Users] where [Name] = @name and [Password] = @password";
+
+
+
+        public UserRepository()
+        {
+
+            _UserConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["connectionStringName"].ConnectionString);
+        }
+
+
+        public UserModel FindUser(UserModel user)
+        {
+            using (_UserConnection)
+            {
+                SqlCommand command = new SqlCommand(_FindUserQuery, _UserConnection);
+                command.Parameters.AddWithValue("@name", user.Name);
+                command.Parameters.AddWithValue("@password", user.Password);
+
+                try
+                {
+                    _UserConnection.Open();
+                    var reader = command.ExecuteReader();
+
+                    if (reader != null) return user;
+
+                }
+                catch (SqlException e)
+                {
+                    Console.Out.Write($"SQL Exception {e.Message}");
+                    return new UserModel();
+                }
+                finally
+                {
+                    _UserConnection.Close();
+                }
+
+
+            }
+            return new UserModel();
+
+        
+        }
+
+    }
+        
+    
+}
